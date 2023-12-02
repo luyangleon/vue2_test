38dd8c943be6f090e47a5347abe65c4a633f65c8
vue2_test <luyangleon@qq.com>
2023-12-02 17:20:22 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import { Button, Select } from 'element-ui';
 //关闭vue生产提示
 Vue.config.productionTip = false
 
-Vue.component(Button.name, Button);
-Vue.component(Select.name, Select);
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
 
 new Vue({
   render: h => h(App),
